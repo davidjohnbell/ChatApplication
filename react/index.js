a8b184cae733c51db908924de22ca2d055e0a137
david <david_bell@live.ca>
2018-03-09 22:33:41 -0700

    installing a differect react library

--- react/index.js
+++ react/index.js
@@ -1,1 +1,21 @@
-document.write('hello world');
+// index.js
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import {SideNav, SideNavItem, Navbar, Dropdown, Button} from 'react-materialize';
+
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+            
+            <SideNav>
+                <SideNavItem subheader>sub header</SideNavItem>
+            </SideNav>
+        </div>
+    );
+  }
+}
+
+const root = document.querySelector('#app');
+ReactDOM.render(<App />, root);
