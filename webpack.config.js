48ddc4ce33a68e8929fad9de50c25a4c06e5470e
david <david_bell@live.ca>
2018-03-09 21:59:49 -0700

    updated config files

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,21 @@ module.exports = options => {
         path: __dirname + '/site/',
         filename: 'bundle.js',
       },
+      module: {
+        rules: [
+          {
+            test: /.js$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'babel-loader',
+                options: {
+                  cacheDirectory: true,
+                },
+              },
+            ],
+          },
+        ],
+      },  
     }
   }
